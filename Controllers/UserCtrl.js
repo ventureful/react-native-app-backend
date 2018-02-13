d8ce401230655d636abedb61292e7755d3002ecf
ventureful <hades0122@outlook.com>
2018-02-12 22:00:03 -0400

    Ultimos cambios

--- Controllers/UserCtrl.js
+++ Controllers/UserCtrl.js
@@ -1,7 +1,8 @@
 module.exports = {
   Register: _Register,
   Login: _Login,
-  GetResults: _GetResults
+  GetResults: _GetResults,
+  GetStats: _GetStats
 }
 
 function _Register (req, res) {
@@ -16,10 +17,39 @@ function _GetResults (req, res) {
   let results = [{
     date: Date.now(),
     list: {
-      m: [11,12,13],
-      t: [23,23,43],
-      n: [12,43,54]
+      m: ['aguila','ardilla','ballena'],
+      t: ['cienpies', 'culebra', 'gallina'],
+      n: ['gato','mono','chivo']
+    }
+  },{
+    date: new Date(Date.now() - 24*3600000).getTime(),
+    list: {
+      m: ['chivo','mono','ballena'],
+      t: ['gato', 'gallina', 'gallina'],
+      n: ['culebra','ardilla','cienpies']
     }
   }];
   res.status(200).jsonp({message: results});
+}
+
+function _GetStats (req, res) {
+  let stats = {
+    lastWeek: { 
+      stat: [0,1,3,0,1,0,4]
+    },
+    lastMonth: {
+      stat: [...Array(30).keys()].map((e) => {
+        return Math.floor(Math.random()*3)
+      })
+    },
+    byDayOfWeek: {
+      times: 10,
+      period: '20 weeks'
+    },
+    byPosition: {
+      times: 3,
+      period: '20 weeks'
+    }       
+  };
+  res.status(200).jsonp({message: stats});
 }
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,5 @@ var express = require('express'),
 router.route('/Login').post(UserCtrl.Login);
 router.route('/Register').post(UserCtrl.Register);
 router.route('/GetResults').get(UserCtrl.GetResults);
+router.route('/GetStats').get(UserCtrl.GetStats);
 module.exports = router;
